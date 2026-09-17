1fdaf327e78f04644d6c9c05be816f02c988549f
tashkidd1 <tashcubes@gmail.com>
2026-09-17 12:32:14 +0200

    fix: remove unused GoalCard import

--- src/components/goals/GoalCard.jsx
+++ src/components/goals/GoalCard.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { formatCurrency } from "@/lib/format";
